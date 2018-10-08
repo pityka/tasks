283cef5eeada0e1de8d295cc6b348a131635d1cc
Istvan Bartha <bartha.pityu@gmail.com>
2018-10-08 11:43:25 +0200

    Add stack trace elements to the Throwable circe codec

--- core/src/main/scala/tasks/wire/package.scala
+++ core/src/main/scala/tasks/wire/package.scala
@@ -20,10 +20,29 @@ package object wire {
           .leftMap(t => "ActorRef"))
 
   implicit val throwableEncoder: Encoder[Throwable] =
-    Encoder.encodeString.contramap[Throwable](_.getMessage)
+    Encoder
+      .encodeTuple2[String, List[(String, String, String, Int)]]
+      .contramap[Throwable] { throwable =>
+        (throwable.getMessage,
+         throwable.getStackTrace.toList.map(
+           stackTraceElement =>
+             (stackTraceElement.getClassName,
+              stackTraceElement.getMethodName,
+              stackTraceElement.getFileName,
+              stackTraceElement.getLineNumber)))
+      }
   implicit val throwableDecoder: Decoder[Throwable] =
-    Decoder.decodeString.emap(str =>
-      Either.catchNonFatal(new RuntimeException(str)).leftMap(t => "Throwable"))
+    Decoder.decodeTuple2[String, List[(String, String, String, Int)]].emap {
+      case (msg, stackTrace) =>
+        val exc = new Exception(msg)
+        exc.setStackTrace(stackTrace.map {
+          case (cls, method, file, line) =>
+            new java.lang.StackTraceElement(cls, method, file, line)
+        }.toArray)
+        Either
+          .catchNonFatal(exc)
+          .leftMap(t => "Throwable")
+    }
 
   implicit val fileEncoder: Encoder[File] =
     Encoder.encodeString.contramap[File](_.getAbsolutePath)
