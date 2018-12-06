75ceb9d5ddda1d45d2b1f54df01ece34e51e2aa1
Istvan Bartha <bartha.pityu@gmail.com>
2018-12-06 15:35:20 +0100

    Do not write empty Options into serialized representation
    This ensures schema evolution.

--- core/src/main/scala/tasks/circesupport/circe.scala
+++ core/src/main/scala/tasks/circesupport/circe.scala
@@ -3,10 +3,12 @@ import tasks.queue._
 import com.typesafe.scalalogging.StrictLogging
 
 package object circesupport extends StrictLogging {
-  import io.circe.{Encoder, Decoder}
+  import io.circe.{Encoder, Decoder, Printer}
+  private val printer = Printer.noSpaces.copy(dropNullValues = true)
+
   implicit def serializer[A](implicit enc: Encoder[A]): Serializer[A] =
     new Serializer[A] {
-      def apply(a: A) = enc(a).noSpaces.getBytes("UTF-8")
+      def apply(a: A) = printer.pretty(enc(a)).getBytes("UTF-8")
     }
   implicit def deserielizer[A](implicit dec: Decoder[A]): Deserializer[A] =
     new Deserializer[A] {
---
+++ core/src/test/scala/tasks/SchemaEvolutionViaOptionsTest.scala
@@ -0,0 +1,121 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2018 Istvan Bartha
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package tasks
+
+import org.scalatest._
+
+import org.scalatest.Matchers
+
+import tasks.circesupport._
+import scala.concurrent.Future
+import io.circe.generic.semiauto._
+import com.typesafe.config.ConfigFactory
+
+object SchemaEvolutionViaOptionsTest extends TestHelpers {
+
+  val sideEffect = scala.collection.mutable.ArrayBuffer[String]()
+
+  val testConfig2 = {
+    val tmp = tasks.util.TempFile.createTempFile(".temp")
+    tmp.delete
+    ConfigFactory.parseString(
+      s"""tasks.fileservice.storageURI=${tmp.getAbsolutePath}
+    hosts.numCPU=4
+    tasks.createFilePrefixForTaskId = false
+    """
+    )
+  }
+
+  case class Input1(data1: Int)
+  object Input1 {
+    implicit val enc = deriveEncoder[Input1]
+    implicit val dec = deriveDecoder[Input1]
+  }
+
+  case class Input2(data1: Int, data2: Option[Int])
+  object Input2 {
+    implicit val enc = deriveEncoder[Input2]
+    implicit val dec = deriveDecoder[Input2]
+  }
+
+  val task1 = AsyncTask[Input1, Int]("schemaevolutionviaoptions", 1) {
+    input => implicit computationEnvironment =>
+      sideEffect += "execution of task"
+      Future(1)
+  }
+
+  val task2 = AsyncTask[Input2, Int]("schemaevolutionviaoptions", 1) {
+    input => implicit computationEnvironment =>
+      sideEffect += "execution of task"
+      Future(1)
+  }
+
+  def run = {
+    val run1 = withTaskSystem(testConfig2) { implicit ts =>
+      import scala.concurrent.ExecutionContext.Implicits.global
+
+      val future = for {
+        t1 <- task1(Input1(1))(ResourceRequest(1, 500))
+      } yield t1
+
+      await(future)
+
+    }
+
+    val run2 = withTaskSystem(testConfig2) { implicit ts =>
+      import scala.concurrent.ExecutionContext.Implicits.global
+
+      val future = for {
+        t1 <- task2(Input2(1, None))(ResourceRequest(1, 500))
+      } yield t1
+
+      await(future)
+
+    }
+
+    val run3 = withTaskSystem(testConfig2) { implicit ts =>
+      import scala.concurrent.ExecutionContext.Implicits.global
+
+      val future = for {
+        t1 <- task2(Input2(1, Some(2)))(ResourceRequest(1, 500))
+      } yield t1
+
+      await(future)
+
+    }
+
+    run1.get + run2.get + run3.get
+  }
+
+}
+
+class SchemaEvolutionViaOptionsTestSuite extends FunSuite with Matchers {
+
+  test("schema evolution via options should work") {
+    SchemaEvolutionViaOptionsTest.run shouldBe 3
+    SchemaEvolutionViaOptionsTest.sideEffect.count(_ == "execution of task") shouldBe 2
+  }
+
+}
