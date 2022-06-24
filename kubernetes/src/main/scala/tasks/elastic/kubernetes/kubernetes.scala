c516ad4c538142c0b0d96ca72fe50c9aa47b4bed
Istvan Bartha <106101+pityka@users.noreply.github.com>
2022-06-24 17:55:46 +0200

    Fix divisor

--- kubernetes/src/main/scala/tasks/elastic/kubernetes/kubernetes.scala
+++ kubernetes/src/main/scala/tasks/elastic/kubernetes/kubernetes.scala
@@ -243,7 +243,7 @@ trait K8SHostConfigurationImpl extends HostConfigurationFromConfig {
 
   override lazy val availableMemory = Option(
     System.getenv(config.kubernetesRamLimitEnvVar)
-  ).map(v => (v.toLong / 1000).toInt).getOrElse(config.hostRAM)
+  ).map(v => (v.toLong / 1000000).toInt).getOrElse(config.hostRAM)
 
   override lazy val availableScratch = Option(
     System.getenv(config.kubernetesScratchLimitEnvVar)
