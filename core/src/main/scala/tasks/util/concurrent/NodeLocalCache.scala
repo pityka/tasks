5f3518d95decdb48e269938fe581b8936c2d232d
Istvan Bartha <106101+pityka@users.noreply.github.com>
2022-06-29 23:49:06 +0200

    Add a data structure for reference counted reentrant resources

---
+++ core/src/main/scala/tasks/util/concurrent/NodeLocalCache.scala
@@ -0,0 +1,126 @@
+package tasks.util.concurrent
+
+import cats.effect._
+import cats.instances.list._
+import cats.syntax.all._
+
+object NodeLocalCache {
+
+  case class State[A](
+      map: Map[
+        String,
+        (Option[((A, IO[Unit]), Int)], List[Deferred[IO, (A, IO[Unit])]])
+      ]
+  ) {}
+
+  def make[A] = Ref.of[IO, State[A]](State(Map.empty))
+
+  def offer[A](
+      id: String,
+      orElse: Resource[IO, A],
+      stateR: Ref[IO, State[A]]
+  ): Resource[IO, A] = {
+
+    sealed trait Command
+    case object YouShouldSetIt extends Command
+    case class GiveFromCache(a: A) extends Command
+    case object Wait extends Command
+
+    def newOfferer(
+        state: State[A],
+        id: String,
+        offerer: Deferred[IO, (A, IO[Unit])]
+    ) =
+      state.map.get(id) match {
+        case None =>
+          State(state.map.updated(id, (None, List(offerer)))) -> YouShouldSetIt
+
+        case Some((Some((_, 0)), _)) =>
+          throw new IllegalStateException(
+            "Counter of a non-empty resource can't be zero."
+          )
+        case Some((Some((a, counter)), offerers)) =>
+          State(state.map.updated(id, (Some((a, counter + 1)), offerers))) ->
+            GiveFromCache(a._1)
+
+        case Some((None, offerers)) =>
+          (
+            State(state.map.updated(id, (None, offerer :: offerers))),
+            Wait
+          )
+      }
+
+    val decrement: IO[Unit] =
+      stateR.modify { case State(map) =>
+        map.get(id) match {
+          case None =>
+            throw new IllegalStateException("Decrement never seen resource")
+          case Some((None, _)) =>
+            throw new IllegalStateException(
+              "Decrement not yet computed resource"
+            )
+          case Some((Some((_, 1)), _ :: _)) =>
+            throw new IllegalStateException(
+              "Decrement a resource with a counter and waiting offerers."
+            )
+          case Some((Some(((_, release), 1)), Nil)) =>
+            State(map.removed(id)) -> release
+          case Some((Some(((v, release), counter)), offerers)) =>
+            State(
+              map.updated(id, (Some(((v, release), counter - 1)), offerers))
+            ) -> IO.unit
+        }
+      }.flatten
+
+    val runAllocator =
+      orElse.allocated.flatMap { case a @ (value, _) =>
+        stateR.modify { case State(map) =>
+          val (newState, list) = map.get(id) match {
+            case None =>
+              throw new IllegalStateException(
+                "Running allocator on never seen resource"
+              )
+            case Some((Some(_), _)) =>
+              throw new IllegalStateException(
+                "Running allocator on already available resource"
+              )
+            case Some((None, offerers)) =>
+              (
+                State(
+                  map.updated(id, (Some((a, offerers.size)), Nil))
+                ),
+                offerers
+              )
+          }
+          val action: IO[Unit] =
+            list.traverse(_.complete((value, decrement))).void
+          (newState, action)
+        }.flatten
+      }
+
+    val allocator = Deferred[IO, (A, IO[Unit])].flatMap { offerer =>
+      Async[IO].uncancelable { _ => // `poll` used to embed cancelable code
+        stateR
+          .modify { state =>
+            val (newState, command) = newOfferer(state, id, offerer)
+
+            val action = command match {
+              case YouShouldSetIt =>
+                runAllocator
+              case GiveFromCache(a) =>
+                offerer.complete((a, decrement)).void
+              case Wait => IO.unit
+            }
+
+            (newState, action)
+          }
+          .flatten
+          .flatMap(_ => offerer.get)
+
+      }
+
+    }
+
+    Resource(allocator)
+  }
+}
---
+++ core/src/test/scala/tasks/util/nodelocalcache.test.scala
@@ -0,0 +1,103 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland,
+ * Group Fellay
+ * Modified work, Copyright (c) 2016 Istvan Bartha
+
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package tasks.util
+
+import org.scalatest.funsuite.{AnyFunSuite => FunSuite}
+import tasks.util.concurrent.NodeLocalCache
+
+import cats.effect._
+import cats.effect.implicits._
+import cats.instances.all._
+import cats.effect.unsafe.implicits.global
+import scala.util.Random
+import scala.concurrent.duration._
+
+class CatsNodeLocalCacheTest extends FunSuite {
+
+  test("cats-effect based node local cache ") {
+    val program: IO[(List[(Int, Int)], Set[Int])] = for {
+      garbage <- Ref.of[IO, Set[Int]](Set.empty)
+      nlc <- NodeLocalCache.make[Int]
+      s <- Random
+        .shuffle((0 until 100).toList)
+        .parTraverseN(100)(i =>
+          IO {
+            NodeLocalCache
+              .offer(
+                "id" + i / 10,
+                Resource.make(IO {
+                  println(s"allocating $i")
+                  i
+                }.delayBy(Random.nextLong(5000).millis))(i =>
+                  IO.println(s"dealloc $i") *> garbage.update(_ + i)
+                ),
+                nlc
+              )
+              .use(i =>
+                IO { println(s"using $i"); i }
+                  .delayBy(Random.nextLong(5000).millis)
+              )
+          }.delayBy(Random.nextLong(5000).millis).flatten.map(r => (i, r))
+        )
+      _ <- IO.println("==================")
+      s2 <- Random
+        .shuffle((0 until 100).toList)
+        .parTraverseN(100)(i =>
+          IO {
+            NodeLocalCache
+              .offer(
+                "id" + i / 10,
+                Resource.make(IO {
+                  println(s"allocating $i")
+                  i
+                }.delayBy(Random.nextLong(5000).millis))(i =>
+                  IO.println(s"dealloc $i") *> garbage.update(_ + i)
+                ),
+                nlc
+              )
+              .use(i =>
+                IO { println(s"using $i"); i }
+                  .delayBy(Random.nextLong(5000).millis)
+              )
+          }.delayBy(Random.nextLong(5000).millis).flatten.map(r => (i, r))
+        )
+      g <- garbage.get
+    } yield (s ++ s2, g)
+
+    val (result, garbage) = program.unsafeRunSync()
+    val sorted = result.sortBy(_._1).map(_._2)
+    assert(sorted.size == 200)
+    sorted.grouped(20).toList.zipWithIndex.foreach { case (group, idx) =>
+      assert(group.distinct.size > 0)
+      assert(group.distinct.size <= 2)
+      assert(group.forall(g => garbage.contains(g)))
+      assert(group.forall(g => g >= (idx * 10) && g < (idx + 1) * 10))
+    }
+  }
+
+}
