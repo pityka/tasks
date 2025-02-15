52f15d3d2f593083a3599fbde5fb0fdf0b556ca8
Scala Steward <scala_steward@virtuslab.com>
2025-02-15 15:30:31 +0000

    Reformat with scalafmt 3.8.6
    Executed command: scalafmt --non-interactive

--- core/src/main/scala/tasks/elastic/package.scala
+++ core/src/main/scala/tasks/elastic/package.scala
@@ -3,14 +3,17 @@ package tasks
 import tasks.util.config.TasksConfig
 import cats.effect._
 package object elastic {
-  def makeElasticSupport(implicit config: TasksConfig): Resource[IO,Option[ElasticSupport]] =
+  def makeElasticSupport(implicit
+      config: TasksConfig
+  ): Resource[IO, Option[ElasticSupport]] =
     config.elasticSupport match {
       case ""         => Resource.pure(None)
       case "NOENGINE" => Resource.pure(None)
       case reflective =>
-          tasks.util
-            .reflectivelyInstantiateObject[ElasticSupportFromConfig](reflective)
-            .apply(config).map(Some(_))
+        tasks.util
+          .reflectivelyInstantiateObject[ElasticSupportFromConfig](reflective)
+          .apply(config)
+          .map(Some(_))
 
     }
 }
