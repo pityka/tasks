9d3458d866419d32931f8295b69eb1207d6bf77e
Istvan Bartha <106101+pityka@users.noreply.github.com>
2025-03-23 18:21:19 +0100

    Ensure postgres transaction isolation level is serializable, add loop, add tests

--- postgres/src/test/scala/tasks/test.scala
+++ postgres/src/test/scala/tasks/test.scala
@@ -38,8 +38,74 @@ import tasks.queue.QueueImpl
 import tasks.queue.Launcher.LauncherActor
 import tasks.util.message.Address
 import cats.effect.kernel.Ref
-
+import skunk.implicits._
+import skunk.codec.all._
+import skunk.data.TransactionIsolationLevel
+import skunk.data.TransactionAccessMode
 class PostgresSuite extends FunSuite with Matchers {
+  import natchez.Trace.Implicits.noop
+  test("skunk transaction") {
+    skunk.Session
+      .single[IO](
+        host = "localhost",
+        port = 5432,
+        user = "postgres",
+        database = "postgres",
+        password = Some("mysecretpassword")
+      )
+      .use { session1 =>
+        skunk.Session
+          .single[IO](
+            host = "localhost",
+            port = 5432,
+            user = "postgres",
+            database = "postgres",
+            password = Some("mysecretpassword")
+          )
+          .use { session2 =>
+            Ref.of[IO, Boolean](false).flatMap { ref =>
+              val prep = session1.execute(
+                sql"create table if not exists testtx (value text)".command
+              ) *>
+                session1.execute(sql"insert into testtx values ('a')".command)
+              val tx =
+                (prep *> (session1
+                  .transaction(
+                    TransactionIsolationLevel.Serializable,
+                    TransactionAccessMode.ReadWrite
+                  )
+                  .use { tx1 =>
+                    session2
+                      .transaction(
+                        TransactionIsolationLevel.Serializable,
+                        TransactionAccessMode.ReadWrite
+                      )
+                      .use { _ =>
+                        (session1
+                          .execute(sql"select * from testtx".query(text)) *>
+                          session2
+                            .execute(sql"select * from testtx".query(text)) *>
+                          session1
+                            .execute(
+                              sql"update testtx set value = 'b'".command
+                            ) *>
+                          tx1.commit *>
+                          session2
+                            .execute(
+                              sql"update testtx set value = 'c'".command
+                            )).map(b => ref.set(true))
+                      }
+                  }))
+
+              IO.uncancelable { poll =>
+                poll(tx).flatten
+              }.attempt
+                .flatMap(_ => ref.get.map(result => assert(!result)))
+            }
+          }
+      }
+      .unsafeRunSync()
+  }
 
   test("postgres connects") {
     tasks.queue.Postgres
@@ -52,17 +118,59 @@ class PostgresSuite extends FunSuite with Matchers {
         password = Some("mysecretpassword")
       )
       .use { tx =>
-        Ref.of[IO,Boolean](false).flatMap{ ref =>
-        tx.get *>
-          tx.flatModify(old =>
-            old.update(
-              QueueImpl.LauncherJoined(LauncherActor(Address("sdfa")))
-            ) -> ref.set(true)) *> tx.get.map(state =>
-            assert(state == State.empty.update(
-              QueueImpl.LauncherJoined(LauncherActor(Address("sdfa")))
-            ))
-          ) *> ref.get.map(a => assert(a))
+        Ref.of[IO, Boolean](false).flatMap { ref =>
+          tx.get *>
+            tx.flatModify(old => State.empty -> IO.unit) *>
+            tx.flatModify(old =>
+              old.update(
+                QueueImpl.LauncherJoined(LauncherActor(Address("sdfa")))
+              ) -> ref.set(true)
+            ) *> tx.get.map(state =>
+              assert(
+                state == State.empty.update(
+                  QueueImpl.LauncherJoined(LauncherActor(Address("sdfa")))
+                )
+              )
+            ) *> ref.get.map(a => assert(a))
         }
+      }
+      .unsafeRunSync()
+  }
+  test("postgres concurrent flatmodify") {
+    tasks.queue.Postgres
+      .makeTransaction(
+        table = "table1",
+        host = "localhost",
+        port = 5432,
+        user = "postgres",
+        database = "postgres",
+        password = Some("mysecretpassword")
+      )
+      .use { tx1 =>
+        tasks.queue.Postgres
+          .makeTransaction(
+            table = "table1",
+            host = "localhost",
+            port = 5432,
+            user = "postgres",
+            database = "postgres",
+            password = Some("mysecretpassword")
+          )
+          .use { tx2 =>
+            Ref.of[IO, Int](0).flatMap { ref =>
+              val io1 = tx1.flatModify(old =>
+                old.update(
+                  QueueImpl.LauncherJoined(LauncherActor(Address("AAA")))
+                ) -> ref.update(_ + 1)
+              )
+              val io2 = tx2.flatModify(old =>
+                old.update(
+                  QueueImpl.LauncherJoined(LauncherActor(Address("BBB")))
+                ) -> ref.update(_ + 1)
+              )
+              io1.both(io2) *> ref.get.map(a => assert(a == 2))
+            }
+          }
       }
       .unsafeRunSync()
   }
