36515c5a154d013a7bf1e48e7aaafa889d65cd4d
Istvan Bartha <106101+pityka@users.noreply.github.com>
2026-09-11 11:49:36 +0200

    simplify mainprocessresourcetest

--- core/src/test/scala/tasks/MainProcessResourceTest.scala
+++ core/src/test/scala/tasks/MainProcessResourceTest.scala
@@ -26,61 +26,43 @@ import cats.effect.unsafe.implicits.global
 import org.ekrich.config.{Config, ConfigFactory}
 import scala.concurrent.duration._
 
-import tasks.deploy._
-import tasks.elastic.ElasticSupport
-import tasks.elastic.process.LocalShellElasticSupport
-import tasks.elastic.process.ShellConfig
 import tasks.queue.QueueImpl
 import tasks.util.Transaction
-import tasks.util.config.TasksConfig
 
 object MainProcessResourceTest {
 
-  def config: Config = {
+  private def storageFolder = {
     val tmp = tasks.util.TempFile.createTempFile(".temp")
     tmp.delete
-    ConfigFactory.parseString(
-      s"""tasks.fileservice.storageURI=${tmp.getAbsolutePath}
+    tmp.mkdirs
+    tmp.getAbsolutePath
+  }
+
+  def appConfig: Config = ConfigFactory.parseString(
+    s"""tasks.fileservice.storageURI=$storageFolder
       tasks.cache.enabled = false
       hosts.numCPU = 1
-      hosts.gpus = []
+      tasks.askInterval = 20 ms
+      tasks.addShutdownHook = false
+      """
+  )
+
+  def workerConfig: Config = ConfigFactory.parseString(
+    s"""tasks.fileservice.storageURI=$storageFolder
+      tasks.cache.enabled = false
+      hosts.numCPU = 1
+      hosts.app = false
+      hosts.master = "localhost:1"
       tasks.askInterval = 20 ms
       tasks.disableRemoting = false
       tasks.addShutdownHook = false
-      tasks.elastic.logQueueStatus = false
       """
-    )
-  }
+  )
 
   def recorder(events: Ref[IO, List[String]]): Resource[IO, Unit] =
     Resource.make(events.update(_ :+ "acquire"))(_ =>
       events.update(_ :+ "release")
     )
-
-  def workerElasticSupport(
-      rawConfig: Config
-  ): Resource[IO, Option[ElasticSupport]] = {
-    val tconfig: TasksConfig =
-      tasks.util.config.parse(() => tasks.util.loadConfig(Some(rawConfig)))
-
-    val workerHostConfig = new DefaultHostConfigurationFromConfig()(tconfig) {
-      override lazy val myRoles: Set[Role] = Set(Worker)
-    }
-
-    Resource
-      .eval(LocalShellElasticSupport.make(ShellConfig(Nil)))
-      .map(base =>
-        Some(
-          new ElasticSupport(
-            hostConfig = Some((_: TasksConfig) => workerHostConfig),
-            shutdownFromNodeRegistry = base.shutdownFromNodeRegistry,
-            shutdownFromWorker = base.shutdownFromWorker,
-            createNodeFactory = base.createNodeFactory,
-            getNodeName = base.getNodeName
-          )
-        )
-      )
-  }
 
 }
 
@@ -92,8 +74,10 @@ class MainProcessResourceTestSuite extends FunSuite with Matchers {
     "the main process resource is open while the body of an app process runs and is closed after it"
   ) {
     val program = Ref.of[IO, List[String]](Nil).flatMap { events =>
-      withTaskSystem(config, recorder(events))(_ => events.get)
-        .flatMap(duringBody => events.get.map(afterBody => (duringBody, afterBody)))
+      withTaskSystem(appConfig, recorder(events))(_ => events.get)
+        .flatMap(duringBody =>
+          events.get.map(afterBody => (duringBody, afterBody))
+        )
     }
 
     val (duringBody, afterBody) = program.unsafeRunSync()
@@ -103,15 +87,13 @@ class MainProcessResourceTestSuite extends FunSuite with Matchers {
   }
 
   test("a worker process does not acquire the main process resource") {
-    val rawConfig = config
-
     val program = for {
       events <- Ref.of[IO, List[String]](Nil)
       queueState <- Ref.of[IO, QueueImpl.State](QueueImpl.State.empty)
       worker = withTaskSystem(
-        config = Some(rawConfig),
+        config = Some(workerConfig),
         s3Client = Resource.pure(None),
-        elasticSupport = workerElasticSupport(rawConfig),
+        elasticSupport = Resource.pure(None),
         externalQueueState =
           Resource.pure(Some(Transaction.fromRef(queueState))),
         meterProvider = Resource.pure[
